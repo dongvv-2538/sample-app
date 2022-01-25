1b7586066929ee699cd55b6b9e9ab32d0af74b6d
= <=>
2022-01-25 15:20:18 +0700

    adding some comment to application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
-
-    def full_title(page_title= '')
-        base_title = "Ruby on Rails Tutorial Sample App"
-        if page_title.empty? 
-            base_title
+    #returns the full title on a per-page basis
+    def full_title(page_title= '')                              #define the function
+        base_title = "Ruby on Rails Tutorial Sample App"        #initialize the base_title variable
+        if page_title.empty?                                    #testing boolean of page_title
+            base_title                                          #if empty, return base_title only
         else
-            "#{page_title} | #{base_title}"
+            "#{page_title} | #{base_title}"                     #otherwise, return page_title | base_title
         end
     end
+
 end
