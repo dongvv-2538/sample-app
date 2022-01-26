816deaf953df7c359248643e8d972cce216fcbcb
= <=>
2022-01-26 14:07:03 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   root 'static_pages#home'
 
   get 'signup'  => 'users#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
