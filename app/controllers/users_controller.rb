9cefb9375143cd5427b59ee030e15b3900bac2af
= <=>
2022-01-27 20:12:52 +0700

    Adding user activation function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save 
-      flash[:success] = "Welcome to the Sample App"
-      log_in @user 
-      redirect_to @user
+      @user.send_activation_email
+      flash[:success] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
